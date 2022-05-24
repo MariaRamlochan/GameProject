b83f42bfba3a39b1cfb416581757336bd0698921
MariaRamlochan <mariaramlochan@hotmail.com>
2022-05-23 20:32:46 -0400

    Added Player Health Bar on the UI

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EnemyCollision : MonoBehaviour
 {
     public int health = 30;
     public int damage = 10;
+    public Image filler;
+
+    // Use this for initialization
+    void Start()
+    {
+        filler.fillAmount = 1.0f;
+    }
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Enemy")
         {
             health = health - damage;
+            filler.fillAmount -= 1.0f/3.0f;
             Debug.Log(health);
 
         }
