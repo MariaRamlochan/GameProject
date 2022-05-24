6e71c70ca16391c2ea0c2e32820fce7b6346cbbc
MariaRamlochan <mariaramlochan@hotmail.com>
2022-05-24 14:21:23 -0400

    Merged the KeySpawn Script with the PointsCollect

--- Assets/Scripts/PointsCollect.cs
+++ Assets/Scripts/PointsCollect.cs
@@ -11,6 +11,7 @@ public class PointsCollect : MonoBehaviour
     public int score = 0;
     public int totalKey = 0;
     public TMP_Text lblScore;
+    public TMP_Text lblTask;
     public AudioSource audioScorce;
     public AudioClip syringeAudio;
     public AudioClip keyAudio;
@@ -18,11 +19,17 @@ public class PointsCollect : MonoBehaviour
     public Sprite syringSprite;
     public Sprite keySprite;
 
+    public GameObject keyPrefab;
+    public Transform spawnPoint;
+    public int maxPoints;
+    public bool isSpawn = false;
+
     private void Start()
     {
         audioScorce = GetComponent<AudioSource>();
         icon.sprite = syringSprite;
-    }
+        lblTask.text = "Avoid The Enemy\nCollect All Syringes";
+}
 
     private void OnTriggerEnter(Collider other)
     {
@@ -31,6 +38,15 @@ public class PointsCollect : MonoBehaviour
             score += 1;
             lblScore.text = "X " + score;
             audioScorce.PlayOneShot(syringeAudio);
+            if (score == maxPoints)
+            {
+                lblTask.text = "Find The Key";
+                if (isSpawn == false)
+                {
+                    Instantiate(keyPrefab, spawnPoint.position, Quaternion.Euler(90, 0, 0));
+                    isSpawn = true;
+                }
+            }
         }
 
         if (other.gameObject.tag == "Key")
@@ -39,6 +55,7 @@ public class PointsCollect : MonoBehaviour
             score += 1;
             icon.sprite = keySprite;
             lblScore.text = score + "/" + totalKey;
+            lblTask.text = "Escape Through The Front Door";
             audioScorce.PlayOneShot(keyAudio);
         }
 
